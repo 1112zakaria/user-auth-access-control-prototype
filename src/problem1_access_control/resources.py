e08bd7a75701d512b0c2853202eba48d1a07ef3d
Zakaria Ismail <1112zakaria@gmail.com>
2023-11-16 17:43:04 -0500

    uaacp-4: added basic enums for resources and roles
    idk if ill use these, im just brainstorming

---
+++ src/problem1_access_control/resources.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class Resources(Enum):
+    ACCOUNT_BALANCE                     = 1,
+    INVESTMENT_PORTFOLIO                = 2,
+    FINANCIAL_ADVISOR_CONTACT_DETAILS   = 3,
+    INVESTMENT_ANALYST_CONTACT_DETAILS  = 4,
+    MARKET_INSTRUMENTS                  = 5,
+    PRIVATE_CONSUMER_INSTRUMENTS        = 6,
+    
---
+++ src/problem1_access_control/roles.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class Roles(Enum):
+    CLIENT              = 1,
+    PREMIUM_CLIENT      = 2,
+    EMPLOYEE            = 3,
+    ELEVATED_EMPLOYEE   = 4,
+    FINANCIAL_PLANNER   = 5,
+    INVESTMENT_ANALYST  = 6,
+    FINANCIAL_ADVISOR   = 7,
+
+
