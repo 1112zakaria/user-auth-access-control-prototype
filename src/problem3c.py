4d596da12f2b637e3b546fdd47f934c85b01fa9c
Zakaria Ismail <1112zakaria@gmail.com>
2023-12-01 17:46:37 -0500

    uaacp-12: added some password check implementations and enums

--- src/problem3c.py
+++ src/problem3c.py
@@ -1,3 +1,4 @@
+from enum import Enum, auto
 import getpass
 import pprint
 import json
@@ -10,6 +11,21 @@ import problem4a
 
 # Problem 3c - Implement the enrolment mechanism and proactive password checker
 STATUS = 'status'
+MIN_PASSWORD_SIZE = 8
+SPECIAL_CHAR_SET = {'!','@','#','$','%','?','*'}
+
+class PasswordCheckResult(Enum):
+    SUCCESS                 = 'SUCCESS'
+    TOO_FEW_CHARS           = f'Too few characters. Password must be at least {MIN_PASSWORD_SIZE} characters in length.'
+    MISSING_SPECIAL_CHAR    = f'Password must include at least one special character from the set {SPECIAL_CHAR_SET}'
+    MISSING_UPPERCASE_CHAR  = 'Password must include at least one upper-case letter'
+    MISSING_LOWERCASE_CHAR  = 'Password must include at least one lower-case letter'
+    MISSING_NUMERICAL_DIGIT = 'Password must include at least one numerical digit'
+    COMMON_WEAK_PASSWORD    = 'Password is found to be a common weak password.'
+    MATCHES_COMMON_NUMBER   = 'Password matches the format of calendar dates, license plate numbers, telephone numbers, or other common number.'
+    MATCHES_USERID          = 'Password matches the user ID.'
+
+
 
 ## CLIENT
 def enrol_user_interface():
@@ -49,6 +65,46 @@ def enrol_user_server():
         print("User exists")
         return {STATUS: False}
     return {STATUS: True}
+
+
+def check_character_length(password: str) -> PasswordCheckResult:
+    if len(password) > MIN_PASSWORD_SIZE:
+        return PasswordCheckResult.SUCCESS
+    return PasswordCheckResult.TOO_FEW_CHARS
+
+def check_special_characters(password: str) -> PasswordCheckResult:
+    # check the following:
+    # - 1 upper case
+    # - 1 lower case
+    # - 1 digit
+    # - 1 special character from special char set
+    errors: list[PasswordCheckResult] = []
+    if not any(c for c in password if c.islower()):
+        errors += PasswordCheckResult.MISSING_LOWERCASE_CHAR
+    if not any(c for c in password if c.isupper()):
+        errors += PasswordCheckResult.MISSING_UPPERCASE_CHAR
+    if not any(c for c in password if c.isnumeric()):
+        errors += PasswordCheckResult.MISSING_NUMERICAL_DIGIT
+    if not any(c for c in password if c in SPECIAL_CHAR_SET):
+        errors += PasswordCheckResult.MISSING_SPECIAL_CHAR
+    
+    if errors == []:
+        return PasswordCheckResult.SUCCESS
+    return errors
+
+def check_weak_common_passwords(password: str) -> PasswordCheckResult:
+    pass
+
+def check_common_numbers(password: str) -> PasswordCheckResult:
+    pass
+
+def check_user_id_match(username: str, password: str) -> PasswordCheckResult:
+    pass
+
+def perform_proactive_password_check(password: str):
+    pass
+
+
 
 
 if __name__ == "__main__":
